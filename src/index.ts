d183600c4eafe5c6ad14b2dcae4fcbf3107911ec
Kevin Frei <kevinfrei@hotmail.com>
2020-10-11 21:50:27 -0700

    Initial simple concept code

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
 // Nothing to see here
 
 export const NothignToSeeHere = 1;
+
+export declare type IterExpr<T> = {
+  map: <U>(arg: (v: T) => U) => IterExpr<U>;
+  forEach: (arg: (v: T) => void) => void;
+  filter: (arg: (v: T) => boolean) => IterExpr<T>;
+};
+/*
+function Iter<T>(col: Iterable<T>): IterExpr<T> {
+  return { map: () => {}, forEach: () => {}, filter: () => {} };
+}
+*/

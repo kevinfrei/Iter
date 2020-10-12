87ed2f2f75a00a9e20066f32fde81eb07e89d9ee
Kevin Frei <kevinfrei@hotmail.com>
2020-10-11 21:56:13 -0700

    A terrible initial implementation of forEach

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,23 @@
-// Nothing to see here
-
-export const NothignToSeeHere = 1;
-
 export declare type IterExpr<T> = {
-  map: <U>(arg: (v: T) => U) => IterExpr<U>;
   forEach: (arg: (v: T) => void) => void;
-  filter: (arg: (v: T) => boolean) => IterExpr<T>;
+  // filter: (arg: (v: T) => boolean) => IterExpr<T>;
+  // map: <U>(arg: (v: T) => U) => IterExpr<U>;
 };
-/*
-function Iter<T>(col: Iterable<T>): IterExpr<T> {
-  return { map: () => {}, forEach: () => {}, filter: () => {} };
+
+export function Iter<T>(
+  col: Iterable<T>,
+  dontCacheResults?: boolean,
+): IterExpr<T> {
+  const results: T[] | null = dontCacheResults ? null : [];
+  // TODO: Make a helper to hide the cached results
+  function helper(): Iterable<T> {
+    return results || [...col];
+  }
+  function forEach(arg: (v: T) => void) {
+    // This is NOT the right way to walk over the Iterable if it's been cached
+    for (const v of helper()) {
+      arg(v);
+    }
+  }
+  return { forEach };
 }
-*/

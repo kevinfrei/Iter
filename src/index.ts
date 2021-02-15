ddffee8ac3b125b7cddb45d2c66b0716a0ec18e1
Kevin Frei <kevinfrei@hotmail.com>
2021-02-15 15:34:12 -0800

    Trying to make a more usable iteration thing

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 export declare type IterExpr<T> = {
   forEach: (arg: (v: T) => void) => void;
   // filter: (arg: (v: T) => boolean) => IterExpr<T>;
@@ -21,3 +23,31 @@ export function Iter<T>(
   }
   return { forEach };
 }
+
+function MakeArray(count: number): number[] {
+  const data: number[] = Array(count) as number[];
+  for (let i = 0; i < data.length; i++) {
+    data[i] = Math.round(Math.random() * 100);
+  }
+  return data;
+}
+
+// How do I want to use this stuff?
+
+function ArrayUsage() {
+  const data = MakeArray(15000000);
+  //  const datas = data.map((e) => [e, e + 1, e + 2, e + 3, e + 4]);
+  //  const appends = [].concat(...datas);
+  const tmp = data.filter((d) => d % 2 === 0);
+  const sum = tmp.reduce((pv, cv) => pv + cv);
+  console.log(sum);
+}
+/*
+function IterUsage() {
+  const data = MakeHugeIter(15000000);
+  const tmp = data.filter((d) => d % 2 === 0);
+  const sum = tmp.reduce((pv, cv) => pv + cv);
+}
+*/
+
+ArrayUsage();
